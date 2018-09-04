3d6d8ebeeff15c23f6c28ec644f89fc759f2d4f2
MatheusPSS <matheuspereirasilvasouza@outlook.com>
2018-09-04 14:41:27 -0300

    IMC
    Tela do app

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by GrupoCheck on 04/09/2018.
+//  Copyright © 2018 Matheus Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
